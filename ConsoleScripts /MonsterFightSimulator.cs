915289fef6853cd6600054e9576277e2e45c3cb0
GrantelbartonCetamine <138975843+GrantelbartonCetamine@users.noreply.github.com>
2024-10-23 15:23:45 +0200

    Update MonsterFightSimulator.cs

--- ConsoleScripts /MonsterFightSimulator.cs
+++ ConsoleScripts /MonsterFightSimulator.cs
@@ -1,3 +1,4 @@
+
 ﻿using System;
 
 class MonsterFight()
@@ -7,11 +8,6 @@ class MonsterFight()
     class Main()
     {
         public Ork ork = new Ork(100, 20, "Nahkampf", "Starker Angriif von Oben", 30, "etwas");
-
-        public Archer archer = new Archer(100, 15, "Range", "Aufgeladener schuss", 25, "can heal 5 healthpoint");
-
-        public Goblin goblin = new Goblin(100, 5, "Nahkampf", "Assozialer Angriff in den Rücken", 15, "can go Invisible and connot be Attacked while this Time");
-
         public Troll troll = new Troll(100, 25, "Nahkampf", "Sehr Starker Angriff", 40, "10% Armor");
 
     }
@@ -33,37 +29,6 @@ class MonsterFight()
         }
     }
 
-    public class Archer(int health, int DamageDealing, string type, string SpecialAttackMove, int SpecialAttackDamage, string passive)
-    {
-        int Health = health;
-        int Damage = DamageDealing;
-        int SpecialAttackDamage = SpecialAttackDamage;
-
-        string fighttype = type;
-        string SpecialAttackMove = SpecialAttackMove;
-        string passs = passive;
-
-        public override string ToString(){
-
-            return "Archer";
-        }
-    }
-
-    public class Goblin(int health, int DamageDealing, string type, string SpecialAttackMove, int SpecialAttackDamage, string passive)
-    {
-        int Health = health;
-        int Damage = DamageDealing;
-        int SpecialAttackDamage = SpecialAttackDamage;
-
-        string fighttype = type;
-        string SpecialAttackMove = SpecialAttackMove;
-        string passs = passive;
-
-        public override string ToString(){
-
-            return "Goblin";
-        }
-    }
 
     public class Troll(int health, int DamageDealing, string type, string SpecialAttackMove, int SpecialAttackDamage, string passive)
     {
@@ -92,23 +57,44 @@ class GameLogic : MonsterFight
     }
 
     static void StartGame()
-    {
+    {      
+        int classcounter = 0;
+
         var Ork = new MonsterFight.Ork(100, 20, "Nahkampf", "Starker Angriff von Oben", 30, "etwas");
-        var Archer = new MonsterFight.Archer(100, 15, "Range", "Aufgeladener Schuss", 25, "kann 5 Lebenspunkte heilen");
-        var Goblin = new MonsterFight.Goblin(100, 5, "Nahkampf", "Angriff in den Rücken", 15, "kann unsichtbar werden");
         var Troll = new MonsterFight.Troll(100, 25, "Nahkampf", "Sehr starker Angriff", 40, "10% Rüstung");
 
         Console.WriteLine("Choose 2 Charackters");
-        Console.WriteLine($"Available Classes : {Ork}(1) , {Archer}(2) ,{Goblin}(3) ,{Troll}(4)");
+        Console.WriteLine($"Available Classes : {Ork}(1) , {Troll}(2)");
 
-        Console.WriteLine("..Press any key to start the game...");
+        var userChoice = Console.ReadKey();
 
-        Console.ReadLine();
+        while (classcounter < 1)
+        { 
+        switch(userChoice.KeyChar){
 
-        //if (UserCharackterChoose == "1")
+            case '1':
+            Console.WriteLine($" You Choose {Ork}");
+            classcounter ++;
+            break;
 
+            case '2':
+            Console.WriteLine($" You Choose {Troll}");
+            classcounter ++;
+            break;
+            }
 
+            if (classcounter > 1){
+                Console.WriteLine("Cannot choose more Charackters");
+            }
+
+        }
     }
+    
+    static void fight(){
+
+        
+    }
+
 
 
 }
