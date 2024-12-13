598c563cd4c2679482a81878024f507c627bba07
GrantelbartonCetamine <138975843+GrantelbartonCetamine@users.noreply.github.com>
2024-12-13 11:30:38 +0100

    Update 3D Controlls.cs

--- Unity /3D Controlls.cs
+++ Unity /3D Controlls.cs
@@ -5,32 +5,31 @@ public class My3DMovement : MonoBehaviour
     [SerializeField] float PlayerSprintSpeed = 20f;
     [SerializeField] float PlayerSpeed = 10f;
     [SerializeField] float JumpHeight = 15f;
-
-
-
-    Rigidbody Player;
+    [SerializeField] float RaycastSize = 10f;
 
     private CharackterMovement Move;
     private Vector3 ForceDirection = Vector3.zero;
+    private int GroundLayer = 1<<6;
+    private float DoJump;
 
+    Rigidbody Player;
 
 
     void Start()
     {
-        
+
     }
 
     private void Awake()
     {
         Player = GetComponent<Rigidbody>();
         Move = new CharackterMovement();
-
-
+        DoJump = Move.JumpMap.Jump.ReadValue<float>();
     }
 
     private void FixedUpdate()
     {
-        CharackterControlls();
+
 
     }
 
@@ -38,43 +37,51 @@ public class My3DMovement : MonoBehaviour
     {
         Vector3 move = Move.MovementMap.Movement.ReadValue<Vector3>();
         float sprint = Move.SprintMap.Sprint.ReadValue<float>();
-        
+
         if (sprint > 0)
         {
-            Player.velocity = new Vector3(move.x * PlayerSprintSpeed, Player.velocity.y,  move.z * PlayerSprintSpeed);
+            Player.velocity = new Vector3(move.x * PlayerSprintSpeed, Player.velocity.y, move.z * PlayerSprintSpeed);
         }
         else
         {
-             Player.velocity =  new Vector3(move.x * PlayerSpeed, Player.velocity.y , move.z * PlayerSpeed);
+            Player.velocity = new Vector3(move.x * PlayerSpeed, Player.velocity.y, move.z * PlayerSpeed);
         }
 
     }
 
-
-
+    private void Update()
+    {
+        CharackterControlls();
+        IsGrounded();
+        Jump();
+    }
+    
     void Jump()
     {
-        if (IsGrounded())
+        if (IsGrounded() && Move.JumpMap.Jump.WasPressedThisFrame()) 
         {
-            ForceDirection += Vector3.up * JumpHeight
+            Player.velocity = new Vector3(Player.velocity.x , JumpHeight, Player.velocity.z);
         }
     }
 
     private bool IsGrounded()
     {
-        Ray ray = new Ray(Player.transform * Vector3.up * 0.5, Vector3.down);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+
+        Vector3 RayOrigin = Player.transform.position + Vector3.up * 0.1f;
+
+        if (Physics.Raycast(RayOrigin, Vector3.down, out RaycastHit hit, RaycastSize, GroundLayer))
+        {
+            Debug.DrawRay(RayOrigin, Vector3.down * RaycastSize, Color.green);
+            Debug.Log("IsGrounded");
             return true;
-        else 
+        }
+        else
+        {
+            Debug.DrawRay(RayOrigin, Vector3.down * RaycastSize, Color.red);
+            Debug.Log("IsNotGrounded");
             return false;
-
+        }
     }
-
-
-
-
-
-
 
 
 
@@ -87,6 +94,4 @@ public class My3DMovement : MonoBehaviour
     {
         Move.Disable();
     }
-
-
 }
