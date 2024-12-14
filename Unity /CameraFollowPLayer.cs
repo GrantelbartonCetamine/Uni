1440933dc83a2621930427b6bc59a0db559f85d1
GrantelbartonCetamine <138975843+GrantelbartonCetamine@users.noreply.github.com>
2024-12-14 12:45:51 +0100

    Update CameraFollowPLayer.cs

--- Unity /CameraFollowPLayer.cs
+++ Unity /CameraFollowPLayer.cs
@@ -1,18 +1,41 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollowPlayerScript : MonoBehaviour
 {
 
-    private float cameraFollowSpeed = 2f;
+    [SerializeField] Transform CameraTarget;
+    [SerializeField] float CameraFollowSpeed = 10f;
+    [SerializeField] float CameraRotationSpeed = 5f;
+
+    private Vector3 CameraOffset;
+    private Quaternion rotation;
 
 
-    public Transform playerTransform;
-
-    void Update()
+    void Start()
     {
+        CameraOffset = transform.position - CameraTarget.position;
+        rotation = transform.rotation;
+    }
 
-        Vector3 targetPosition = new Vector3(playerTransform.position.x, playerTransform.position.y, -20f);
-        transform.position = Vector3.Slerp(transform.position, targetPosition, cameraFollowSpeed * Time.deltaTime);
+    private void LateUpdate()
+    {
+        CameraFollow();
+        CameraRotaion();
+
+    }
+
+    void CameraFollow()
+    {
+        Vector3 TargetPosition = CameraTarget.position + CameraTarget.rotation * CameraOffset;
+        transform.position = Vector3.Lerp(transform.position, TargetPosition, CameraFollowSpeed * Time.deltaTime);
+    }
+
+    void CameraRotaion()
+    {
+        Quaternion rotation = Quaternion.LookRotation(CameraTarget.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation ,rotation , CameraFollowSpeed * Time.deltaTime);
     }
 }
