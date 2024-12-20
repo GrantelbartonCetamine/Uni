792cdbbf1278cecae83bbd0161885b40b4e3a32a
GrantelbartonCetamine <138975843+GrantelbartonCetamine@users.noreply.github.com>
2024-12-20 14:55:09 +0100

    Update SortingAlgorhytm.cs

--- ConsoleScripts /SortingAlgorhytm.cs
+++ ConsoleScripts /SortingAlgorhytm.cs
@@ -1,66 +1,116 @@
 using System;
 using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection;
+using System.Collections;
 
 class SortierAlgorhytmen
 {
+
     static void Main()
     {
-        InitializeRandomNumbers();
-        AscendingAlgo();
+        List<int> numbers = InitializeRandomNumbers();
+        Decide(numbers);
     }
 
-    static List<int> InitializeRandomNumbers()
+    static List<int> InitializeRandomNumbers()   // Give a list with Inputet Numbers Back
     {
-
         List<int> Numbers = new List<int>();
-        Console.WriteLine($"Enter Some Numbers");
+        Console.WriteLine("Enter numbers (type 'exit' to finish):");
 
         while (true)
         {
             string UserInput = Console.ReadLine();
-            if (UserInput == null)
+            if (UserInput.ToLower() == "exit")
             {
-                Console.WriteLine("Please Enter a Valid Number");
-                continue;
+                break;
             }
-            else if (int.TryParse(UserInput, out int numbers))
+            else if (int.TryParse(UserInput, out int number))
             {
-
-                Numbers.Add(numbers);
-                Console.WriteLine($"Added number {numbers} to List");
-
-                foreach (int i in Numbers)
-                {
-                    Console.WriteLine($"List : {i}");
-                }
+                Numbers.Add(number);
+                Console.WriteLine($"Added number {number} to the list.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
             }
         }
         return Numbers;
     }
 
-    static void AscendingAlgo(List<int> MyList)
-    {   
-
-
-        do 
-        for (int i = 0 ; i < MyList.Count - 1; i++)
+    static void BubbleGum(List<int> MyList)
+    {
+        bool isSorted;
+        do
         {
-            for (int a = 0; a < MyList.Count - i; a++)
+            isSorted = true;
+            for (int i = 0; i < MyList.Count - 1; i++)
             {
-                if (MyList[a] > MyList[a + 1])
+                if (MyList[i] > MyList[i + 1]) // checks if the next index is bigger then my current index
                 {
-                    int PlaceHolder = MyList[a];
-                    MyList[a] = MyList[j + 1];
-                    MyList[j + 1] = PlaceHolder;
-
+                    int PlaceHolder = MyList[i];
+                    MyList[i] = MyList[i + 1]; // checks if the next index is bigger then my current index
+                    MyList[i + 1] = PlaceHolder; // 
+                    isSorted = false;
                 }
             }
-        }while (true);
+        } while (!isSorted);
+    }
+
+
+    static void QuickSortAlgo(List<int> list, int low, int high)
+    {
+        if (low < high)
+        {
+            int index = Decision(list, low, high);
+            QuickSortAlgo(list, low, index - 1);
+            QuickSortAlgo(list, index + 1, high);
+
+        }
+    }
+
+    static int Decision(List<int> list, int low, int high)
+    {
+        int boundary = list[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+
+            if (list[j] <= boundary)
+            {
+                i++;
+                int placeholder = list[i];
+                list[i] = list[j];
+                list[j] = placeholder;
+            }
+        }
+        int temp = list[i + 1];
+        list[i + 1] = list[high];
+        list[high] = temp;
+        return i + 1;
+
 
     }
 
+    static void Decide(List<int> numbers)
+    {
+
+        Console.WriteLine("Choose between an Algorithm (1 for BubbleGum, 2 for QuickSort):");
+        string userinput = Console.ReadLine();
+        if (userinput == "1")
+        {
+            BubbleGum(numbers);
+        }
+        else if (userinput == "2")
+
+        {
+            QuickSortAlgo(numbers, 0, numbers.Count - 1);
+        }
+
+        foreach (var num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
 }
-
-
-
-
