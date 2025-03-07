738a8830171857088c76909a00c175a2633afd71
GrantelbartonCetamine <138975843+GrantelbartonCetamine@users.noreply.github.com>
2025-03-07 10:38:12 +0100

    Update EscapeRoom.cs

--- ConsoleScripts /EscapeRoom.cs
+++ ConsoleScripts /EscapeRoom.cs
@@ -169,9 +169,9 @@ class EscapeRoom
     {   // Greet Player and Explain Controlls
         public static void PlayerGreeting()
         {
-            Console.WriteLine("This is a EscapeRoom \nThe Goal is to find the Exit Good Luck");
             Console.Write("You can Press W for going Forward\nA for going Left\nS for going Back \nAnd D for going to the Right");
             Console.WriteLine("Or you can use Numpad or Arrow buttons");
+            Console.WriteLine("This is a EscapeRoom \nThe Goal is to find the Exit Good Luck");
         }
     }
 
